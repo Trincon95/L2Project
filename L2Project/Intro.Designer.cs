cbfa91d68cdfddbfd53f9de79e49d02e2b632ada
Tania Rincon <tanrincon95@gmail.com>
2018-09-13 10:16:20 -0400

    SimpleDesign

--- L2Project/Intro.Designer.cs
+++ L2Project/Intro.Designer.cs
@@ -30,33 +30,20 @@ namespace L2Project
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.templatedropdown = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.yourname = new System.Windows.Forms.Button();
             this.ticketnum = new System.Windows.Forms.Button();
             this.Client = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.IndianRed;
-            this.button1.Font = new System.Drawing.Font("Modern No. 20", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(42, 323);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(239, 75);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Create New";
-            this.button1.UseVisualStyleBackColor = false;
             // 
             // templatedropdown
             // 
             this.templatedropdown.AllowDrop = true;
+            this.templatedropdown.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(241)))), ((int)(((byte)(242)))));
             this.templatedropdown.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.templatedropdown.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.templatedropdown.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.templatedropdown.FormattingEnabled = true;
             this.templatedropdown.Items.AddRange(new object[] {
             "Suspension",
@@ -74,22 +61,6 @@ namespace L2Project
             this.templatedropdown.Enter += new System.EventHandler(this.templatedropdown_Enter);
             this.templatedropdown.Leave += new System.EventHandler(this.templatedropdown_Leave);
             // 
-            // label1
-            // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Stencil", 20F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.label1.Location = new System.Drawing.Point(119, 40);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(485, 47);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Template Automation ";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.label1.Click += new System.EventHandler(this.label1_Click_1);
-            // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.DimGray;
@@ -106,52 +77,66 @@ namespace L2Project
             // 
             // yourname
             // 
+            this.yourname.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(214)))), ((int)(((byte)(226)))));
             this.yourname.Font = new System.Drawing.Font("Modern No. 20", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.yourname.ForeColor = System.Drawing.Color.Firebrick;
-            this.yourname.Location = new System.Drawing.Point(58, 110);
+            this.yourname.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.yourname.Location = new System.Drawing.Point(46, 110);
             this.yourname.Name = "yourname";
             this.yourname.Size = new System.Drawing.Size(139, 33);
             this.yourname.TabIndex = 6;
             this.yourname.Text = "Your Name";
-            this.yourname.UseVisualStyleBackColor = true;
+            this.yourname.UseVisualStyleBackColor = false;
             // 
             // ticketnum
             // 
+            this.ticketnum.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(214)))), ((int)(((byte)(226)))));
             this.ticketnum.Font = new System.Drawing.Font("Modern No. 20", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ticketnum.ForeColor = System.Drawing.Color.Firebrick;
-            this.ticketnum.Location = new System.Drawing.Point(240, 133);
+            this.ticketnum.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ticketnum.Location = new System.Drawing.Point(221, 110);
             this.ticketnum.Name = "ticketnum";
-            this.ticketnum.Size = new System.Drawing.Size(132, 33);
+            this.ticketnum.Size = new System.Drawing.Size(138, 33);
             this.ticketnum.TabIndex = 7;
             this.ticketnum.Text = "Ticket #INC";
-            this.ticketnum.UseVisualStyleBackColor = true;
+            this.ticketnum.UseVisualStyleBackColor = false;
             this.ticketnum.Click += new System.EventHandler(this.button4_Click);
             // 
             // Client
             // 
+            this.Client.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(214)))), ((int)(((byte)(226)))));
             this.Client.Font = new System.Drawing.Font("Modern No. 20", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Client.ForeColor = System.Drawing.Color.Firebrick;
-            this.Client.Location = new System.Drawing.Point(410, 110);
+            this.Client.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Client.Location = new System.Drawing.Point(395, 110);
             this.Client.Name = "Client";
             this.Client.Size = new System.Drawing.Size(138, 33);
             this.Client.TabIndex = 8;
             this.Client.Text = "Client Name";
-            this.Client.UseVisualStyleBackColor = true;
+            this.Client.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(214)))), ((int)(((byte)(226)))));
+            this.button1.Font = new System.Drawing.Font("Modern No. 20", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Location = new System.Drawing.Point(46, 399);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 33);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Create New\r\n";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Intro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.BackColor = System.Drawing.Color.DarkGray;
-            this.ClientSize = new System.Drawing.Size(578, 444);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(241)))), ((int)(((byte)(242)))));
+            this.ClientSize = new System.Drawing.Size(573, 530);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.Client);
             this.Controls.Add(this.ticketnum);
             this.Controls.Add(this.yourname);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.templatedropdown);
-            this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Intro";
@@ -160,18 +145,16 @@ namespace L2Project
             this.Text = "Intro";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox templatedropdown;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button yourname;
         private System.Windows.Forms.Button ticketnum;
         private System.Windows.Forms.Button Client;
+        private System.Windows.Forms.Button button1;
 
         public EventHandler Yournamebox_TextChanged { get; private set; }
     }
